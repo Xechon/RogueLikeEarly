b323b1f79a3da1c317d41647debfc93135c1f563
Xechon <guess96@gmail.com>
2014-03-30 14:03:21 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@
  */
 public class Main{
 //hello
+    int a = 0;
     public static void main(String[] args){
         System.out.println("please work");
-        //hi
+    }
+    public int getA(){
+     return a;
     }
     public static int testing(){
         return 5;
