7c7b06988026b59636b21b5f9c833631879b6811
Dustin <dustinroepsch@gmail.com>
2014-03-30 14:25:34 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Dustin
  * Date: 3/30/14
  * Time: 1:22 PM
  */
-public class Main{
-//hello
-    int a = 0;
+public class Main extends JFrame{
+    public Main(){
+        super("Test");
+        setSize(new Dimension(500,500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationByPlatform(true);
+
+
+    }
     public static void main(String[] args){
-        System.out.println("please work");
-    }
-    public int getA(){
-     return a;
-    }
-    public static int testing(){
-        return 5;
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    Main main = new Main();
+                    main.setVisible(true);
+                }
+            });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
