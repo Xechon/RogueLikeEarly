e1a30f8a27dd13b187ec0bfacc8fb0269ca4f57e
Dustin <dustinroepsch@gmail.com>
2014-03-31 20:46:21 -0500

    This runs really slow...

--- src/RoomContainer.java
+++ src/RoomContainer.java
@@ -15,6 +15,7 @@ public class RoomContainer extends JPanel {
     }
     @Override
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         Actor[][] actors = room.getActors();
         for (int i = 0; i < actors.length; i++){
